cbbf2bdc8f33c0d4b47834e6cefbea00c8d3ae5f
thomashaile <gthomashaile@gmail.com>
2020-02-28 17:21:57 +0100

    adding functions save and reset

--- handlers/save-number.js
+++ handlers/save-number.js
@@ -10,10 +10,41 @@ function saveNumberHandler() {
 
   // re-render the user interface with values stored in state
 
+const oldBiggest = document.getElementById('biggest').innerHTML ;
+const oldSmallest = document.getElementById('smallest').innerHTML;
+const curr = document.getElementById('input').value;
+const past = numbers.current;
+
+//numbers.current.push(numbers.current);
+if(curr > oldBiggest){
+  numbers.current = curr;
+  numbers.biggest = curr;
+  numbers.smallest = oldSmallest;
+  document.getElementById('input').value = curr;
+  document.getElementById('biggest').innerHTML = curr;
+  document.getElementById('smallest').innerHTML = oldSmallest;
+}
+else if(curr < oldBiggest && curr > oldSmallest){
+  numbers.current = curr;
+  numbers.biggest = oldBiggest;
+  numbers.smallest = oldSmallest;
+  document.getElementById('input').value = curr;
+  document.getElementById('biggest').innerHTML = oldBiggest;
+  document.getElementById('smallest').innerHTML = oldSmallest;
+}
+else if(curr < oldBiggest && curr < oldSmallest){
+  numbers.current = curr;
+  numbers.biggest = oldBiggest;
+  numbers.smallest = curr;
+  document.getElementById('input').value = curr;
+  document.getElementById('biggest').innerHTML = oldBiggest;
+  document.getElementById('smallest').innerHTML = curr;
+}
+
+
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
-    newInput,
-    numbers: JSON.parse(JSON.stringify(numbers))
-  })
+    numbers: JSON.parse(JSON.stringify(curr))
+  });
 }
