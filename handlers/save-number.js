08d09d8e59bfb56c4f064a47e6cc0fb4ea4365ba
Thomas Haile <59530856+thomashaile@users.noreply.github.com>
2020-03-03 13:57:17 +0100

    Update save-number.js

--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,26 +1,19 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
+  
+  const curr = document.getElementById('input').value;
 
   // read from state the data you will need for the next step
+  const oldBiggest = document.getElementById('biggest').innerHTML ;
+  const oldSmallest = document.getElementById('smallest').innerHTML;
+  const past = numbers.current;
 
-  // find the new biggest and smallest values
-
-  // update state: new biggest, new smallest, new current & save the last current
-
-  // re-render the user interface with values stored in state
-
-const oldBiggest = document.getElementById('biggest').innerHTML ;
-const oldSmallest = document.getElementById('smallest').innerHTML;
-const curr = document.getElementById('input').value;
-const past = numbers.current;
-
-//numbers.current.push(numbers.current);
+  // find the new biggest and smallest values  
 if(curr > oldBiggest){
   numbers.current = curr;
   numbers.biggest = curr;
   numbers.smallest = oldSmallest;
-  document.getElementById('input').value = curr;
   document.getElementById('biggest').innerHTML = curr;
   document.getElementById('smallest').innerHTML = oldSmallest;
 }
@@ -28,7 +21,6 @@ else if(curr < oldBiggest && curr > oldSmallest){
   numbers.current = curr;
   numbers.biggest = oldBiggest;
   numbers.smallest = oldSmallest;
-  document.getElementById('input').value = curr;
   document.getElementById('biggest').innerHTML = oldBiggest;
   document.getElementById('smallest').innerHTML = oldSmallest;
 }
@@ -36,10 +28,17 @@ else if(curr < oldBiggest && curr < oldSmallest){
   numbers.current = curr;
   numbers.biggest = oldBiggest;
   numbers.smallest = curr;
-  document.getElementById('input').value = curr;
   document.getElementById('biggest').innerHTML = oldBiggest;
   document.getElementById('smallest').innerHTML = curr;
 }
+
+
+  // update state: new biggest, new smallest, new current & save the last current
+
+  // re-render the user interface with values stored in state
+ document.getElementById('input').value = curr;
+
+
 
 
   // log user interaction: handler name, user input, new state
